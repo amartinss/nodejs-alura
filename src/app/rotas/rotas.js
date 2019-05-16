c14b77ab842a51be5014be8293f25ba082c2926f
Alexandre <amartins.dev@gmail.com>
2019-05-16 08:24:59 -0300

    Aplicando DAO

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,23 +1,34 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
 module.exports = (app) => {
     app.get('/', function (req, resp) {
         resp.marko(require('../views/home.marko'));
     });
 
     app.get('/livros', function (req, resp) {
-        resp.marko(
-            require('../views/livros/lista/lista.marko'),
-            {
-                livros: [
-                    {
-                        id: 1,
-                        titulo: 'Fundamentos do Node'
-                    },
-                    {
-                        id: 2,
-                        titulo: 'Node Avançado'
-                    }
-                ]
-            }
-        );
+        const livroDao = new LivroDao(db);
+        
+        livroDao.lista(function(error, results) {
+            resp.marko(
+                require('../views/livros/lista/lista.marko'),
+                {
+                    livros: results
+                }
+            );
+        });
+
+
+
+        // db.all('SELECT * FROM livros', function(error, results) {
+        //     resp.marko(
+        //         require('../views/livros/lista/lista.marko'),
+        //         {
+        //             livros: results
+        //         }
+        //     );
+        // });
+
+        
     });
 };
